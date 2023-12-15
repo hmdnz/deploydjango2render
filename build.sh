186c7d57c7f8c8cb1673c4f9265f26fe8c4cbbc2
hmdnz <hammadmcferri@gmail.com>
2023-12-15 22:29:29 +0100

    first commit

---
+++ build.sh
@@ -0,0 +1,8 @@
+# install dependencies
+pip install -r deps.txt
+
+# Run Migrations
+python manage.py migrate
+
+### Make the file executable by running this command in the terminal
+# chmod+x build.sh

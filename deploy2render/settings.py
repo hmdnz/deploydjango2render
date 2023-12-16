eea0d37f69e168d2a916d9268a2d314487b9860c
hmdnz <hammadmcferri@gmail.com>
2023-12-16 23:59:31 +0100

    'cors'

--- deploy2render/settings.py
+++ deploy2render/settings.py
@@ -81,11 +81,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "deploy2render.wsgi.application"
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "https://yourfrontenddomain.com",
-    # Add more allowed origins as needed
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # CORS_ALLOWED_ORIGINS = True

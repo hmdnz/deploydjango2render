d7bc4b1487fd3978a9050d1bdfe46e354b714db2
hmdnz <hammadmcferri@gmail.com>
2023-12-16 23:36:01 +0100

    CORS commit

--- deploy2render/settings.py
+++ deploy2render/settings.py
@@ -82,6 +82,9 @@ TEMPLATES = [
 WSGI_APPLICATION = "deploy2render.wsgi.application"
 
 CORS_ALLOWED_ORIGINS = True
+
+
+# CORS_ALLOWED_ORIGINS = True
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
